7662401b517dcec8d806ccc2cea8fc72585b89a8
Shaun <66428604+Shaun579@users.noreply.github.com>
2021-09-15 22:08:30 +0200

    add main.js

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+//campaign
+require("campaign/planet");
+
+//units
+require("units/statuses");
+
+print("Load Complete!");
+//I dont even know what this does
+
+
+
+
+
+
+/*
+credit(){
+  younggam: "answering tons of question",
+  deltaNedas: "refer to routorio, rtfm",
+  sk7725: "answering tons of question, refer to testers"
+}
+*/
